a1f1cc875ffbdda4fcafb0249e7e66745ba7b36b
Mrlabani <158763270+Mrlabani@users.noreply.github.com>
2024-04-05 22:54:32 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Bot
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("7109933749:AAECk_lZQ5T7D76s3owaRORtGeM7KDFPKG4")
+API_ID = int(os.environ.get("24903318"))
+API_HASH = os.environ.get("266a897e11f3872392f16a43e226c902")
 
 # Authorisation
 AUTH = bool(os.environ.get("AUTH", False))
 AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
 
 # Database (MongoDB)
-DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASE_URL = os.environ.get("mongodb+srv://mrnoobx:DAZCdTczVWyECi04@cluster0.sedgwxy.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Gemini-Bot")
